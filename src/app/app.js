d7bb52551df6e5fd6ecd6c2a31e8672f1e940e17
julianleahy <julianleahynz@gmail.com>
2018-02-15 12:45:19 +1300

    output a tile depending where user clicks

--- src/app/app.js
+++ src/app/app.js
@@ -1,3 +1,38 @@
 import 'styles/normalize.css';
 import 'styles/main.scss';
 
+$(function() {
+
+    let  board = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+    ]
+
+    $('.square').click( function() {
+
+        var cell = $(this).attr('id');
+        var row = parseInt(cell[1]);
+        var col = parseInt(cell[2]);
+
+        board[row][col] = false;
+        outputTile();
+  
+    });
+
+    const outputTile = () => {
+
+        for (var i = 0; i < board.length; i++) {
+            for (var j = 0; j < board[i].length; j++) {
+                switch (board[i][j]) {
+                  case false :
+                    $(`#x${i}${j}`).addClass('cross')
+                       
+                }
+            }
+        }
+
+    }
+    
+});
+
