f743e3a8f7b13a692c7c93141509a7b87a28ef6d
julianleahy <julianleahynz@gmail.com>
2018-02-15 12:52:26 +1300

    add comments

--- src/app/app.js
+++ src/app/app.js
@@ -2,36 +2,39 @@ import 'styles/normalize.css';
 import 'styles/main.scss';
 
 $(function() {
-
+    // default board setup
+    // when player places a tile the corresponding index will be marked as false
+    // computers move will be marked as true
     let  board = [
         [null, null, null],
         [null, null, null],
         [null, null, null]
     ]
 
+
+    // get row and column from squares id
+    // place false in corresponding index on board
     $('.square').click( function() {
 
-        var cell = $(this).attr('id');
-        var row = parseInt(cell[1]);
-        var col = parseInt(cell[2]);
+        const cell = $(this).attr('id');
+        const row = parseInt(cell[1]);
+        const col = parseInt(cell[2]);
 
         board[row][col] = false;
         outputTile();
   
     });
 
+    // loop over board if value is false add a cross
     const outputTile = () => {
-
         for (var i = 0; i < board.length; i++) {
             for (var j = 0; j < board[i].length; j++) {
                 switch (board[i][j]) {
                   case false :
-                    $(`#x${i}${j}`).addClass('cross')
-                       
+                    $(`#x${i}${j}`).addClass('cross')     
                 }
             }
         }
-
     }
     
 });
