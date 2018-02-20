708f5ffa5d62919f2699132b4fb250f5c8c2fc47
julianleahy <julianleahynz@gmail.com>
2018-02-20 21:32:54 +1300

    fix image loader bug in safari

--- webpack.config.js
+++ webpack.config.js
@@ -34,28 +34,7 @@ module.exports = {
                     'file-loader?name=[path][name].[ext]',
                     {
                         loader: 'image-webpack-loader',
-                        options: {
-                            mozjpeg: {
-                                progressive: true,
-                                quality: 80
-                            },
-                            // optipng.enabled: false will disable optipng
-                            optipng: {
-                                enabled: false,
-                            },
-                            pngquant: {
-                                quality: '65-90',
-                                speed: 4
-                            },
-                            gifsicle: {
-                                interlaced: false,
-                            },
-                            // the webp option will enable WEBP
-                            webp: {
-                                quality: 75
-                            }
-                        }
-                    },
+                    }
                 ],
             }
         ]
